5c58ffb65ba98220462a4c145798d3505c0b288e
Ananasaaa <kairyakn@gmail.com>
2025-05-05 20:02:51 +0300

    fix bug with key and ui

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,12 @@
 import './homepage.scss';
-import { useGetSpaceImageQuery } from '../../redux/apiSlice';
+import {
+  useGetSpaceImageQuery,
+  useGetLastSevenImgQuery,
+} from '../../redux/apiSlice';
 import renderError from '../../utils/error/renderError';
 import Loader from '../../components/common/loader/Loader';
 import { useState, useEffect } from 'react';
-
 import PhotoModal from '../../components/common/modal/PhotoModal';
-
-import { fetchLastSevenImg } from '../../utils/fetch/fetchLastSevenImg';
 import TodayImage from '../../components/home/TodayImage';
 import GalleryView from '../../components/home/GalleryView';
 
@@ -18,31 +18,29 @@ const backgroundStyle = {
 };
 
 const Homepage = () => {
-  const { data, isLoading, error } = useGetSpaceImageQuery(undefined);
-
   const [showGallery, setShowGallery] = useState(false);
-  const [galleryPhotos, setGalleryPhotos] = useState<any[]>([]);
-  const [galleryLoading, setGalleryLoading] = useState(false);
-  const [galleryError, setGalleryError] = useState<unknown>(null);
   const [likedPhotos, setLikedPhotos] = useState<string[]>([]);
   const [selectedPhoto, setSelectedPhoto] = useState<any>(null);
+
+  const {
+    data: todayImage,
+    isLoading: todayLoading,
+    error: todayError,
+  } = useGetSpaceImageQuery(undefined);
+
+  const {
+    data: lastSevenPhotos,
+    isLoading: galleryLoading,
+    error: galleryError,
+  } = useGetLastSevenImgQuery(undefined, { skip: !showGallery });
 
   useEffect(() => {
     const savedLikes = JSON.parse(localStorage.getItem('likedPhotos') || '[]');
     setLikedPhotos(savedLikes);
   }, []);
 
-  const handleShowGallery = async () => {
+  const handleShowGallery = () => {
     setShowGallery(true);
-    setGalleryLoading(true);
-    try {
-      const results = await fetchLastSevenImg(process.env.REACT_APP_NASA_KEY!);
-      setGalleryPhotos(results);
-    } catch (err) {
-      setGalleryError(err);
-    } finally {
-      setGalleryLoading(false);
-    }
   };
 
   const handleBack = () => {
@@ -61,7 +59,7 @@ const Homepage = () => {
     localStorage.setItem('likedPhotos', JSON.stringify(updatedLikes));
   };
 
-  if (isLoading) {
+  if (todayLoading) {
     return (
       <div className="homepage" style={backgroundStyle}>
         <Loader />
@@ -69,7 +67,7 @@ const Homepage = () => {
     );
   }
 
-  const errorJSX = renderError(error);
+  const errorJSX = renderError(todayError);
   if (errorJSX) {
     return (
       <div className="homepage" style={backgroundStyle}>
@@ -81,10 +79,10 @@ const Homepage = () => {
   return (
     <div className="homepage" style={backgroundStyle}>
       {!showGallery ? (
-        <TodayImage data={data} onOpenGallery={handleShowGallery} />
+        <TodayImage data={todayImage} onOpenGallery={handleShowGallery} />
       ) : (
         <GalleryView
-          photos={galleryPhotos}
+          photos={lastSevenPhotos || []}
           loading={galleryLoading}
           error={galleryError}
           likedPhotos={likedPhotos}
